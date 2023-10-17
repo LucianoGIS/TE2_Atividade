6648ea9e265bbb3a22ffb2321beb94b5346d20f0
judaharagao <judah.aragao@gmail.com>
2023-10-16 22:09:29 -0300

    model-tipo-transacao added

---
+++ src/main/java/br/com/eureka/eureka/model/TipoTransacao.java
@@ -0,0 +1,24 @@
+package br.com.eureka.eureka.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Table(name="TipoTransacao")
+public class TipoTransacao {
+
+    @Id
+
+    @Column(name = "Nome", length = 100, nullable = false)
+    private String Nome;
+
+    @Column(name = "DataCadastro", nullable = false)
+    private LocalDate DataCadastro;
+
+    @Column(name = "DataAlteracao", nullable = false)
+    private LocalDate DataAlteracao;
+
+}
